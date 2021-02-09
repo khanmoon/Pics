4772a8382827a877fd73b67f954072f02916789c
Mansoor Naseer <mansoor.naseer@hotmail.com>
2021-02-10 03:10:59 +0500

    controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { data: "" };
+  onInputChange(e) {
+    console.log(this);
+    this.setState({ data: e.target.value.toUpperCase() });
+  }
+  constructor(props) {
+    console.log(props);
+    super(props);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <label className="field">Image Search</label>
-          <input type="text" />
+          <label className="field">{this.state.data}</label>
+          <input
+            value={this.state.data}
+            type="text"
+            onChange={this.onInputChange}
+          />
         </form>
       </div>
     );
